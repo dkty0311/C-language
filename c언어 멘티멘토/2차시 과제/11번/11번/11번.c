ac03dcb9e911487be1da39d5c9dc8c40397d75f9
dkty0311 <dkty0311@naver.com>
2019-11-17 04:07:47 +0900

    모두 커밋

---
+++ c언어 멘티멘토/2차시 과제/11번/11번/11번.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int a[100] = { 0 };
+	int x, ten = 0, one = 0;
+
+	for (int i = 0; i < 100; i++)
+	{
+
+		printf("ют╥б");
+		scanf_s("%d", &x);
+		if (x = 0)
+		{
+
+		}
+		else if(x>10)
+		{
+			ten++;
+		}
+		else if(0<x<)
+	}
+}
