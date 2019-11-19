0cc54117b46d480f54c8febfb7da04d2b3606c6d
dkty0311 <2star4840@naver.com>
2019-11-19 13:57:42 +0900

    모두 커밋

---
+++ c언어 멘티멘토/2차시 과제/13번/13번/13번.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int score[5][4] = { 0 };
+	int average[5] = { 0 };
+	int average = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("ÇÐ»ý %d >>", i + 1);
+		scanf_s("%d %d %d %d", &score[i][0], &score[i][1], &score[i][2], &score[i][3]);
+	}
+
+	for(int i=0; i<5;)
+
+}
